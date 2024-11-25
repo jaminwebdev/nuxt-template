c2616a04f2b750ccb2448cf31261eab42bcc1ee0
Jamin <jaminwebdev@gmail.com>
2024-11-25 15:02:38 -0600

    list packages on homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,27 @@
 <template>
   <Container>
-    <h1 class="text-body-text">Color mode: {{ $colorMode.value }}</h1>
+    <h1>What's Included?</h1>
+    <ul class="mt-6 grid gap-2">
+      <li class="flex items-center gap-2">
+        <Icon class='text-teal-400' name="material-symbols:check-circle" size="24px"/> 
+        Nuxt Color mode: <strong>{{ $colorMode.value }}</strong>
+      </li>
+      <li class="flex items-center gap-2">
+        <Icon class='text-teal-400' name="material-symbols:check-circle" size="24px"/> 
+        Nuxt Icons 
+      </li>
+      <li class="flex items-center gap-2">
+        <Icon class='text-teal-400' name="material-symbols:check-circle" size="24px"/> 
+        Nuxt Image 
+      </li>
+      <li class="flex items-center gap-2">
+        <Icon class='text-teal-400' name="material-symbols:check-circle" size="24px"/> 
+        Nuxt Tailwind 
+      </li>
+      <li class="flex items-center gap-2">
+        <Icon class='text-teal-400' name="material-symbols:check-circle" size="24px"/> 
+        Nuxt Shadcn 
+      </li>
+    </ul>
   </Container>
 </template>
